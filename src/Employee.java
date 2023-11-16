9771327f1851e55ec1f5d72829b02267bfe80131
admin-rez <kraken_1111@mail.ru>
2023-11-16 17:55:18 +0300

    kursovay

---
+++ src/Employee.java
@@ -0,0 +1,49 @@
+public class Employee {
+    private static int counter = 0;
+    private int id;
+    private String fullName;
+    private int department;
+    private double salary;
+
+    public Employee(String fullName, int department, double salary) {
+        this.id = ++counter;
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+}
